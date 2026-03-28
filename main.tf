79adb417f73a1dc45ec5c30fe32cd316b6bb9354
brunohenrico96 <120275508+brunohenrico96@users.noreply.github.com>
2026-03-28 14:00:33 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # Aqui configuramos o Backend (onde a memória do Terraform vai ficar)
+  backend "s3" {
+    bucket = "meu-terraform-state-bruno" # Coloque o nome do seu bucket aqui
+    key    = "transfermarkt/terraform.tfstate"
+    region = "us-east-1" # Coloque a sua região
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
