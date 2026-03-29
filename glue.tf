262bdc1b81a38fc13f487a3d349ff2676a48e47b
brunohenrico96 <brunohenrico1996@outlook.com>
2026-03-29 14:02:27 -0300

    Update Glue job dependencies to include pyarrow for encryption properties resolution

--- glue.tf
+++ glue.tf
@@ -16,7 +16,8 @@ resource "aws_glue_job" "extract_transfermarkt" {
   default_arguments = {
     "--TempDir"                     = "s3://${aws_s3_bucket.scripts_bucket.bucket}/temp/"
     # Instalamos as bibliotecas necessárias dinamicamente no momento da execução
-    "--additional-python-modules"   = "awswrangler==3.7.0,beautifulsoup4==4.12.2,requests==2.31.0"
+    # Adicionamos pyarrow para resolver o conflito de encryption_properties
+    "--additional-python-modules"   = "awswrangler==3.9.1,pyarrow==15.0.0,beautifulsoup4==4.12.2,requests==2.31.0"
     # Nosso parâmetro customizado (A seleção que queremos extrair)
     "--selecao_id"                  = "3439" # 3439 é o ID do Brasil na URL deles
     "--database_name"               = aws_glue_catalog_database.transfermarkt_db.name
